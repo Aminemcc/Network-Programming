d36b32de83d1413364a11a8b66668d7007754bf8
Aminemc <aminemc236@gmail.com>
2023-02-25 16:25:41 +0700

    select

--- Pertemuan 3 - Tugas/5025201251_5025201259/client/client_select.py
+++ Pertemuan 3 - Tugas/5025201251_5025201259/client/client_select.py
@@ -3,20 +3,18 @@ import sys
 
 server_ip = "localhost"
 server_port = 6969
+buffer_size = 1024
 
 server_address = (server_ip, server_port)
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect(server_address)
 
-sys.stdout.write('>> ')
-
 try:
     while True:
-        message = sys.stdin.readline()
+        message = input(">> ")
         client_socket.send(bytes(message, 'utf-8'))
-        received_data = client_socket.recv(1024).decode('utf-8')
-        sys.stdout.write(received_data)
-        sys.stdout.write('>> ')
+        received_data = client_socket.recv(buffer_size).decode('utf-8')
+        print(f"Received : {received_data}")
 
 except KeyboardInterrupt:
     client_socket.close()
--- Pertemuan 3 - Tugas/5025201251_5025201259/server/server_select.py
+++ Pertemuan 3 - Tugas/5025201251_5025201259/server/server_select.py
@@ -4,36 +4,54 @@ import sys
 
 server_ip = "localhost"
 server_port = 6969
+buffer_size = 1024
 
-server_address = (server_ip, server_port)
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-server_socket.bind(server_address)
-server_socket.listen(5)
 
-input_socket = [server_socket]
+def send_file(filename):
 
-try:
-    while True:
-        read_ready, write_ready, exception = select.select(input_socket, [], [])
-        
-        for sock in read_ready:
-            if sock == server_socket:
-                client_socket, client_address = server_socket.accept()
-                input_socket.append(client_socket)        
+    pass
+
+
+
+
+def main():
+    server_address = (server_ip, server_port)
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server_socket.bind(server_address)
+    server_socket.listen(5)
+
+    input_socket = [server_socket]
+
+    try:
+        while True:
+            read_ready, write_ready, exception = select.select(input_socket, [], [])
             
-            else:            	
-                try:
-                    data = sock.recv(1024)
-                    print(sock.getpeername(), data.decode())
+            for sock in read_ready:
+                if sock == server_socket:
+                    client_socket, client_address = server_socket.accept()
+                    input_socket.append(client_socket)        
                 
-                    if data:
-                        sock.send(data)
-                    else:                    
-                        sock.close()
-                        input_socket.remove(sock)
-                except:
-                    continue
-except KeyboardInterrupt:        
-    server_socket.close()
-    sys.exit(0)
+                else:            	
+                    try:
+                        data = sock.recv(buffer_size)
+                        data = data.decode()
+                        datas = data.split(" ")
+                        if datas[0] == "download":
+                            send_file(datas[1])
+                        print(datas)
+                    
+                        if data:
+                            sock.send(str.encode(datas[0]))
+                        else:                    
+                            sock.close()
+                            input_socket.remove(sock)
+                    except:
+                        break
+    except KeyboardInterrupt:        
+        server_socket.close()
+        sys.exit(0)
+
+
+if __name__ == "__main__":
+    main()
